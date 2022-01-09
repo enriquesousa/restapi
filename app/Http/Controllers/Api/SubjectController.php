d2bd7a2fcfbab111a4dbdaf6a5a750841f59b3d5
enriquesousa <enrique.sousa@gmail.com>
2022-01-09 00:10:35 -0800

    161. RestAPI Subject CRUD Part 1

---
+++ app/Http/Controllers/Api/SubjectController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Subject;
+use Illuminate\Http\Request;
+
+class SubjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // Con eloquent model
+        $subject = Subject::all();
+        return response()->json($subject);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validateData = $request->validate([
+            'class_id' => 'required',
+            'subject_name' => 'required|unique:subjects|max:25',
+            'subject_code' => 'required|unique:subjects|max:25'
+        ]);
+
+        // Con eloquent model
+        $subject = Subject::create($request->all());
+        return response('Subject Inserted');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\SclassController;
+use App\Http\Controllers\Api\SubjectController;
 
 Route::ApiResource('/class', SclassController::class);
+Route::ApiResource('/subject', SubjectController::class);
