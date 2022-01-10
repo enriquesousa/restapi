f408039937426dc34860f5853373ba755072534e
enriquesousa <enrique.sousa@gmail.com>
2022-01-09 20:39:26 -0800

    163. RestAPI Section CRUD Part 1

---
+++ app/Http/Controllers/Api/SectionController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Section;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class SectionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $section = Section::all();
+        return response()->json($section);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validateData = $request->validate([
+            'class_id' => 'required',
+            'section_name' => 'required'
+        ]);
+
+        $section = Section::create($request->all());
+        return response('Section Inserted');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\Api\SclassController;
 use App\Http\Controllers\Api\SubjectController;
+use App\Http\Controllers\Api\SectionController;
 
 Route::ApiResource('/class', SclassController::class);
 Route::ApiResource('/subject', SubjectController::class);
+Route::ApiResource('/section', SectionController::class);
