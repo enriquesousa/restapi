63236de1c03da38018fb07ac9d1d746f2418e047
enriquesousa <enrique.sousa@gmail.com>
2022-01-11 18:04:21 -0800

    165. RestAPI Student CRUD Part 1

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,9 @@
 use App\Http\Controllers\Api\SclassController;
 use App\Http\Controllers\Api\SubjectController;
 use App\Http\Controllers\Api\SectionController;
+use App\Http\Controllers\Api\StudentController;
 
 Route::ApiResource('/class', SclassController::class);
 Route::ApiResource('/subject', SubjectController::class);
 Route::ApiResource('/section', SectionController::class);
+Route::ApiResource('/student', StudentController::class);
